827e97d58c796f539e11e0a75fe4d433e0eda8ba
Jilly Taboga <54650814+JillyTaboga@users.noreply.github.com>
2022-09-01 17:31:20 -0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.black,
+        primarySwatch: Colors.blue,
       ),
       home: const Scaffold(),
     );
